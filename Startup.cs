b220d67ab29245e75abe26b26d8d076e0baf35c6
George Svitenko <georg.svit@gmail.com>
2021-03-07 00:49:36 +0200

    IInstaller pattern was implemented for DI configuration.

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SmartLocker.WebAPI.Extensions;
 
 namespace SmartLocker.WebAPI
 {
@@ -25,12 +18,7 @@ namespace SmartLocker.WebAPI
 
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmartLocker.WebAPI", Version = "v1" });
-            });
+            services.InstallServicesInAssembly(Configuration);            
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
