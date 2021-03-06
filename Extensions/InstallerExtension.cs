0fb93338ee7bc742bca5e61cd383a33ce24ef909
George Svitenko <georg.svit@gmail.com>
2021-03-07 00:38:09 +0200

    InstallerExtension added. InstallServicesInAssembly method implemented.

---
+++ Extensions/InstallerExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartLocker.WebAPI.ServiceInstallers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Extensions
+{
+    public static class InstallerExtension
+    {
+        public static void InstallServicesInAssembly(this IServiceCollection service, IConfiguration configuration)
+        {
+            Assembly.GetAssembly(typeof(Startup))?
+                .GetTypes()
+                .Where(x => !x.IsInterface && !x.IsAbstract && typeof(IInstaller).IsAssignableFrom(x))
+                .Select(Activator.CreateInstance)
+                .Cast<IInstaller>()
+                .ToList()
+                .ForEach(installer => installer.InstallServices(service, configuration));
+        }
+    }
+}
