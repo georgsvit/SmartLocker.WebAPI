a6c1c9fa36f01279d31281f4d33157026c756dec
George Svitenko <georg.svit@gmail.com>
2021-03-07 00:46:10 +0200

    SwaggerGenInstaller added.

---
+++ ServiceInstallers/Installers/SwaggerGenInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.ServiceInstallers.Installers
+{
+    public class SwaggerGenInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(
+            c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmartLocker.WebAPI", Version = "v1" });
+            });
+        }
+    }
+}
