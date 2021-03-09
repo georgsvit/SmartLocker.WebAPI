24161940e7a7a8982d17b0496e59db6219ea1822
George Svitenko <georg.svit@gmail.com>
2021-03-09 22:24:31 +0200

    Add a service book class to the tool domain entity

---
+++ Domain/ServiceBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SmartLocker.WebAPI.Domain
+{
+    public class ServiceBook
+    {
+        private ServiceBook() { }
+
+        public ServiceBook(Guid id, DateTime lastServiceDate, TimeSpan timeBetweenServices, int maxUsages, int usages)
+        {
+            Id = id;
+            LastServiceDate = lastServiceDate;
+            TimeBetweenServices = timeBetweenServices;
+            MaxUsages = maxUsages;
+            Usages = usages;
+            ToolId = Id;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime LastServiceDate { get; set; }
+        public TimeSpan TimeBetweenServices { get; set; }
+        public int MaxUsages { get; set; }
+        public int Usages { get; set; }
+        public Guid ToolId { get; set; }
+    }
+}
