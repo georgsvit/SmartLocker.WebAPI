13a279ed98ffa8d01e660121d919367b38d83abb
George Svitenko <georg.svit@gmail.com>
2021-03-09 22:08:39 +0200

    Add Roles constants Adding ADMIN, EMPLOYEE, SERVICEMAN roles

---
+++ Domain/Constants/Roles.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartLocker.WebAPI.Domain.Constants
+{
+    public static class Roles
+    {
+        public const string ADMIN = "ADMIN";
+        public const string EMPLOYEE = "EMPLOYEE";
+        public const string SERVICEMAN = "SERVICEMAN";
+
+        public static string[] GetAllRoles() => new string[]
+        {
+            ADMIN,
+            EMPLOYEE,
+            SERVICEMAN
+        };
+    }
+}
