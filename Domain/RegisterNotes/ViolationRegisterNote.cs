2d168c84cb30a4124c8cf57880b3329a70f03198
George Svitenko <georg.svit@gmail.com>
2021-03-09 22:44:09 +0200

    Add class for violation register domain entity

---
+++ Domain/RegisterNotes/ViolationRegisterNote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Domain.RegisterNotes
+{
+    public class ViolationRegisterNote : BaseRegisterNote
+    {
+        private ViolationRegisterNote() { }
+
+        public ViolationRegisterNote(DateTime date, Guid userId, Guid lockerId, Guid toolId) : base(date, userId)
+        {
+            LockerId = lockerId;
+            ToolId = toolId;
+        }
+
+        public Guid LockerId { get; set; }
+        public Guid ToolId { get; set; }
+
+        //
+        public Locker Locker { get; set; }
+        public Tool Tool { get; set; }
+    }
+}
