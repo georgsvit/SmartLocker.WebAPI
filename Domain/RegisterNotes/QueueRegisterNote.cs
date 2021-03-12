b321ede20d8d1a37a9a51d6f1ec92228f0dc52d1
George Svitenko <georg.svit@gmail.com>
2021-03-13 00:04:42 +0200

    Add class for queue register domain entity

---
+++ Domain/RegisterNotes/QueueRegisterNote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Domain.RegisterNotes
+{
+    public class QueueRegisterNote : BaseRegisterNote
+    {
+        private QueueRegisterNote() { }
+
+        public QueueRegisterNote(DateTime date, Guid userId, Guid toolId) : base(date, userId)
+        {
+            ToolId = toolId;
+        }
+
+        public Guid ToolId { get; set; }
+
+        //
+        public Tool Tool { get; set; }
+    }
+}
