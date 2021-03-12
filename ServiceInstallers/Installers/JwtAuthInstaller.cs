4f0f13eab41867a3c56854974965e728b3f1f99e
George Svitenko <georg.svit@gmail.com>
2021-03-13 01:22:44 +0200

    Add jwt auth installer

---
+++ ServiceInstallers/Installers/JwtAuthInstaller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using SmartLocker.WebAPI.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.ServiceInstallers.Installers
+{
+    public class JwtAuthInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            JwtOptions jWTOptions = new();
+            configuration.GetSection(JwtOptions.SectionName).Bind(jWTOptions);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jWTOptions.Issuer,
+
+                        ValidateAudience = true,
+                        ValidAudience = jWTOptions.Audience,
+
+                        ValidateLifetime = true,
+
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = jWTOptions.GetSymmetricSecurityKey()
+                    };
+                });
+        }
+    }
+}
