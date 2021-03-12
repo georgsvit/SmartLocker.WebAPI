17cd95558a41eacfba97b84faf7f0c53712c8633
George Svitenko <georg.svit@gmail.com>
2021-03-13 01:28:04 +0200

    Add ServicesInstaller Register JwtTokenService as transient service

---
+++ ServiceInstallers/Installers/ServicesInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartLocker.WebAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.ServiceInstallers.Installers
+{
+    public class ServicesInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<JwtTokenService>();
+        }
+    }
+}
