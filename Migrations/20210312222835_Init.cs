258d4a14a76325aade05a6f9552028d213b1fea8
George Svitenko <georg.svit@gmail.com>
2021-03-13 00:38:54 +0200

    Add db init migration

---
+++ Migrations/20210312222835_Init.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartLocker.WebAPI.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lockers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsFull = table.Column<bool>(type: "bit", nullable: false),
+                    IsBlocked = table.Column<bool>(type: "bit", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lockers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccessLevel = table.Column<int>(type: "int", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tools",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImgUri = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccessLevel = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LockerId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ServiceBookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tools", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tools_Lockers_LockerId",
+                        column: x => x.LockerId,
+                        principalTable: "Lockers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Tools_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountingRegister",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ToolId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsTaken = table.Column<bool>(type: "bit", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountingRegister", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccountingRegister_Tools_ToolId",
+                        column: x => x.ToolId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QueueRegister",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ToolId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QueueRegister", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QueueRegister_Tools_ToolId",
+                        column: x => x.ToolId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceBooks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LastServiceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TimeBetweenServices = table.Column<TimeSpan>(type: "time", nullable: false),
+                    MaxUsages = table.Column<int>(type: "int", nullable: false),
+                    Usages = table.Column<int>(type: "int", nullable: false),
+                    ToolId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceBooks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiceBooks_Tools_ToolId",
+                        column: x => x.ToolId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceRegister",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ToolId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Cost = table.Column<double>(type: "float", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceRegister", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiceRegister_Tools_ToolId",
+                        column: x => x.ToolId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ViolationRegister",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LockerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ToolId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ViolationRegister", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ViolationRegister_Lockers_LockerId",
+                        column: x => x.LockerId,
+                        principalTable: "Lockers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ViolationRegister_Tools_ToolId",
+                        column: x => x.ToolId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountingRegister_ToolId",
+                table: "AccountingRegister",
+                column: "ToolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QueueRegister_ToolId",
+                table: "QueueRegister",
+                column: "ToolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceBooks_ToolId",
+                table: "ServiceBooks",
+                column: "ToolId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceRegister_ToolId",
+                table: "ServiceRegister",
+                column: "ToolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tools_LockerId",
+                table: "Tools",
+                column: "LockerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tools_UserId",
+                table: "Tools",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ViolationRegister_LockerId",
+                table: "ViolationRegister",
+                column: "LockerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ViolationRegister_ToolId",
+                table: "ViolationRegister",
+                column: "ToolId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountingRegister");
+
+            migrationBuilder.DropTable(
+                name: "QueueRegister");
+
+            migrationBuilder.DropTable(
+                name: "ServiceBooks");
+
+            migrationBuilder.DropTable(
+                name: "ServiceRegister");
+
+            migrationBuilder.DropTable(
+                name: "ViolationRegister");
+
+            migrationBuilder.DropTable(
+                name: "Tools");
+
+            migrationBuilder.DropTable(
+                name: "Lockers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
