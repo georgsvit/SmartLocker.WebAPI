7830e425634b8334e4e5de5a25ac5c4560153d82
George Svitenko <georg.svit@gmail.com>
2021-03-14 00:12:22 +0200

    Add registration request dto

---
+++ Contracts/DTOs/External/Requests/RegistrationRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record RegistrationRequest
+    {
+        public string Login { get; }
+        public string Password { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Role { get; }
+        public int AccessLevel { get; }        
+    }
+}
