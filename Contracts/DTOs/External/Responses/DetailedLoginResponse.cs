9b50f0a6e77a0d0bfda58e8907da7f0e4afeb9d9
George Svitenko <georg.svit@gmail.com>
2021-03-14 00:48:54 +0200

    Add detailed response dto on login request

---
+++ Contracts/DTOs/External/Responses/DetailedLoginResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Responses
+{
+    public record DetailedLoginResponse
+    {
+        public DetailedLoginResponse(Guid id, string firstName, string lastName, string role, string token, DateTime expireDate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Role = role;
+            Token = token;
+            ExpireDate = expireDate;
+        }
+
+        public Guid Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Role { get; }
+
+        public string Token { get; }
+        public DateTime ExpireDate { get; }
+    }
+}
