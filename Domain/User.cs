44e5bf9b6ffddbc99977b4c1f13e24a8b53314e6
George Svitenko <georg.svit@gmail.com>
2021-03-14 01:53:28 +0200

    Add user class public constructor

--- Domain/User.cs
+++ Domain/User.cs
@@ -8,6 +8,17 @@ namespace SmartLocker.WebAPI.Domain
     {
         private User() { }
 
+        public User(string firstName, string lastName, string role, AccessLevel accessLevel, string login, string password)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Role = role;
+            AccessLevel = accessLevel;
+            Login = login;
+            Password = password;
+        }
+
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
