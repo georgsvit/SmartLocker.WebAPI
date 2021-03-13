3d3bac2b35458c11424d5d192ca6bde2b4dc1324
George Svitenko <georg.svit@gmail.com>
2021-03-14 00:53:00 +0200

    Add api routes class

---
+++ Contracts/ApiRoutes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Contracts
+{
+    public static class ApiRoutes
+    {
+        public static class Account
+        {
+            public const string Base = "";
+
+            public const string Login = Base + "/login";
+            public const string Register = Base + "/register";
+        }
+
+    }
+}
