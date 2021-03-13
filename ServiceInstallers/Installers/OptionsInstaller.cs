5ec71ca5ca5fcaa42d588b3bbfcd28ec3b2c1e02
George Svitenko <georg.svit@gmail.com>
2021-03-14 01:51:51 +0200

    Add OptionsInstaller

---
+++ ServiceInstallers/Installers/OptionsInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartLocker.WebAPI.Options;
+
+namespace SmartLocker.WebAPI.ServiceInstallers.Installers
+{
+    public class OptionsInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<JwtOptions>(configuration.GetSection(JwtOptions.SectionName));
+        }
+    }
+}
