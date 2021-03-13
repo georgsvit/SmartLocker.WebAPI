aa0a436e3220c65de90d5a380d29dcd4647971d6
George Svitenko <georg.svit@gmail.com>
2021-03-14 00:48:04 +0200

    Add response on bad request dto

---
+++ Contracts/DTOs/External/Responses/BadRequestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Responses
+{
+    public record BadRequestResponse
+    {
+        public BadRequestResponse(string message) => 
+            (Message) = (message);
+
+        public string Message { get; }
+    }
+}
