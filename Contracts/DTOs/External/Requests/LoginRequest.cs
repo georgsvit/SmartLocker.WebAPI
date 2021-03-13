bd7796305689437f644ba47d1ef8c42185940214
George Svitenko <georg.svit@gmail.com>
2021-03-14 00:02:20 +0200

    Add login request dto

---
+++ Contracts/DTOs/External/Requests/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record LoginRequest
+    {
+        public string Login { get; }
+        public string Password { get; }
+    }
+}
