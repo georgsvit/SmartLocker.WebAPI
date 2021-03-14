614798d6615d4883e7633274bef055952d6ddb02
George Svitenko <georg.svit@gmail.com>
2021-03-14 16:46:37 +0200

    Apply localization use in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using SmartLocker.WebAPI.Extensions;
+using SmartLocker.WebAPI.Options;
+using System.Globalization;
+using System.Linq;
 
 namespace SmartLocker.WebAPI
 {
@@ -21,7 +26,7 @@ namespace SmartLocker.WebAPI
             services.InstallServicesInAssembly(Configuration);
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IOptions<CultureOptions> cultureOptions)
         {
             if (env.IsDevelopment())
             {
@@ -29,6 +34,15 @@ namespace SmartLocker.WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartLocker.WebAPI v1"));
             }
+
+            SetCultureConfigurationValues(cultureOptions, out string defaultCultureName, out CultureInfo[] supportedCultures);
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCultureName),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
 
             app.UseHttpsRedirection();
 
@@ -47,6 +61,14 @@ namespace SmartLocker.WebAPI
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void SetCultureConfigurationValues(
+            IOptions<CultureOptions> options, out string defaultCultureName, out CultureInfo[] supportedCultures)
+        {
+            CultureOptions cultureOptions = options.Value;
+            defaultCultureName = cultureOptions.DefaultCulture;
+            supportedCultures = cultureOptions.SupportedCultures.Select(culture => new CultureInfo(culture)).ToArray();
         }
     }
 }
