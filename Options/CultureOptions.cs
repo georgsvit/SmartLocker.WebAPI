63a4e826920d0a764e926f7480894c7b8562ba4e
George Svitenko <georg.svit@gmail.com>
2021-03-14 15:50:20 +0200

    Add CultureOptions class

---
+++ Options/CultureOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartLocker.WebAPI.Options
+{
+    public class CultureOptions
+    {
+        public const string SectionName = "CultureOptions";
+
+        public string[] SupportedCultures { get; set; }
+        public string DefaultCulture { get; set; }
+    }
+}
