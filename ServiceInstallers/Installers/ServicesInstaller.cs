242de2ab9f136991c6cc2969b4748367bc8fae31
George Svitenko <georg.svit@gmail.com>
2021-03-16 00:07:37 +0200

    Configure DI for UserService

--- ServiceInstallers/Installers/ServicesInstaller.cs
+++ ServiceInstallers/Installers/ServicesInstaller.cs
@@ -2,10 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
 using SmartLocker.WebAPI.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SmartLocker.WebAPI.ServiceInstallers.Installers
 {
@@ -14,6 +10,7 @@ namespace SmartLocker.WebAPI.ServiceInstallers.Installers
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<AccountService>();
+            services.AddTransient<UserService>();
 
             services.AddTransient<JwtTokenService>();
             services.AddTransient<IStringLocalizer, LocalizationService>();
