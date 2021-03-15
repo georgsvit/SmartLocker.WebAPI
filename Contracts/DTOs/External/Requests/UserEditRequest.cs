4a7d0587587d158e8584ac2352296199a8aa70ca
George Svitenko <georg.svit@gmail.com>
2021-03-15 22:46:11 +0200

    Add dto for edit user request

---
+++ Contracts/DTOs/External/Requests/UserEditRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record UserEditRequest(
+        string FirstName,
+        string LastName,
+        string Role,
+        int AccessLevel,
+        string Login,
+        string Password
+        );
+}
