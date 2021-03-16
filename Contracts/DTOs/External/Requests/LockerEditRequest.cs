3509216fcb8c6c6fb0796a5384708871bcb54513
George Svitenko <georg.svit@gmail.com>
2021-03-17 00:02:45 +0200

    Add dto for edit locker request

---
+++ Contracts/DTOs/External/Requests/LockerEditRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record LockerEditRequest(
+        bool IsFull,
+        bool IsBlocked,
+        string Login,
+        string Password
+        );
+}
