d2c6f4c03e1cfe16e25861eebf418c3c3205e32b
George Svitenko <georg.svit@gmail.com>
2021-03-16 19:03:01 +0200

    Add dto for user data

---
+++ Contracts/DTOs/External/Responses/UserDataResponse.cs
@@ -0,0 +1,17 @@
+﻿using SmartLocker.WebAPI.Domain;
+using SmartLocker.WebAPI.Domain.Constants;
+using System;
+using System.Collections.Generic;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Responses
+{
+    public record UserDataResponse(
+        Guid Id,
+        string FirstName,
+        string LastName,
+        string Role,
+        AccessLevel AccessLevel,
+        string Login,
+        List<Tool> Tools
+        );
+}
