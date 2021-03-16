9580ba72d3314d42e5d9c448b4c0668cba2ade78
George Svitenko <georg.svit@gmail.com>
2021-03-17 00:02:05 +0200

    Add dto for locker data

---
+++ Contracts/DTOs/External/Responses/LockerDataResponse.cs
@@ -0,0 +1,14 @@
+﻿using SmartLocker.WebAPI.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Responses
+{
+    public record LockerDataResponse(
+        Guid Id,
+        bool IsFull,
+        bool IsBlocked,
+        string Login,
+        List<Tool> Tools
+        );
+}
