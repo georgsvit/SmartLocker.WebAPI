5a0d23af6e1ba7533f1bb6dd503a18312483341e
George Svitenko <georg.svit@gmail.com>
2021-03-19 00:34:28 +0200

    Add AccountingController Get and Create ViolationNote endpoints implemented

---
+++ Controllers/AccountingController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartLocker.WebAPI.Contracts;
+using SmartLocker.WebAPI.Contracts.DTOs.External.Requests;
+using SmartLocker.WebAPI.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Controllers
+{
+    [ApiController]
+    public class AccountingController : ControllerBase
+    {
+        private readonly AccountingService accountingService;
+
+        public AccountingController(AccountingService accountingService)
+        {
+            this.accountingService = accountingService;
+        }
+
+        [HttpPost(ApiRoutes.Accounting.ViolationNote)]
+        public async Task<IActionResult> AddViolationNote([FromBody] ViolationNoteCreateRequest request)
+        {
+            try
+            {
+                await accountingService.AddViolationNote(request);
+                return Ok();
+            }
+            catch (Exception e)
+            {   
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet(ApiRoutes.Accounting.ViolationNote)]
+        public async Task<IActionResult> GetViolationNote([FromBody] ViolationNoteGetRequest request)
+        {
+            try
+            {
+                var notes = await accountingService.GetViolationRegister(request);
+                return Ok(notes);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
