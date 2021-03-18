6e507a3699818795d39e8df3cda6cf6d00480c5c
George Svitenko <georg.svit@gmail.com>
2021-03-18 15:48:34 +0200

    Add dto for edit service book request

---
+++ Contracts/DTOs/External/Requests/ServiceBookEditRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record ServiceBookEditRequest(
+        DateTime LastServiceDate,
+        int MsBetweenServices,
+        int MaxUsages,
+        int Usages
+        );
+}
