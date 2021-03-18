1bd55e3ce6a0a54e0a8e0f4bfa80f8b2d0ec70d7
George Svitenko <georg.svit@gmail.com>
2021-03-18 18:51:49 +0200

    Add dto for tool edit request

---
+++ Contracts/DTOs/External/Requests/ToolEditRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record ToolEditRequest(
+        string Name,
+        string Description,
+        string ImgUrl,
+        int AccessLevel
+        );
+}
