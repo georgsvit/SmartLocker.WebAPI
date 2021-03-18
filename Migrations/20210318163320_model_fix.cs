7c66150732e3efe2090c43a4324b14cf0140b7a6
George Svitenko <georg.svit@gmail.com>
2021-03-18 18:48:44 +0200

    Update db to new ServiceBook model

---
+++ Migrations/20210318163320_model_fix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartLocker.WebAPI.Migrations
+{
+    public partial class model_fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeBetweenServices",
+                table: "ServiceBooks");
+
+            migrationBuilder.AddColumn<long>(
+                name: "MsBetweenServices",
+                table: "ServiceBooks",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MsBetweenServices",
+                table: "ServiceBooks");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "TimeBetweenServices",
+                table: "ServiceBooks",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+    }
+}
