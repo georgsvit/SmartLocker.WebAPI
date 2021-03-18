125374922b10d74d229f9ce1b3ea4fd170e9b986
George Svitenko <georg.svit@gmail.com>
2021-03-18 18:52:16 +0200

    Add dto for tool create request

---
+++ Contracts/DTOs/External/Requests/ToolCreateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record ToolCreateRequest(
+        string Name,
+        string Description,
+        string ImgUrl,
+        int AccessLevel,
+        DateTime LastServiceDate,
+        long MsBetweenServices,
+        int MaxUsages,
+        int Usages
+        );
+}
