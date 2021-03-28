392fd55edf027ca7e9d6fb4adf1489ce59b70d1b
George Svitenko <georg.svit@gmail.com>
2021-03-28 12:11:07 +0300

    Add dto for ServiceNote get request

---
+++ Contracts/DTOs/External/Requests/ServiceNoteGetRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record ServiceNoteGetRequest(
+        DateTime StartDate,
+        DateTime FinishDate
+        );
+}
