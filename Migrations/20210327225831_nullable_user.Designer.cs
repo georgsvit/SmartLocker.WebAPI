5e0ffe19d73cd99e2a6f986ce17963e56d5560d3
George Svitenko <georg.svit@gmail.com>
2021-03-28 12:07:20 +0300

    Apply migration for model change (nullable user id)

--- Migrations/20210327225831_nullable_user.Designer.cs
+++ Migrations/20210327225831_nullable_user.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartLocker.WebAPI.Data;
 
 namespace SmartLocker.WebAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210327225831_nullable_user")]
+    partial class nullable_user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210327225831_nullable_user.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartLocker.WebAPI.Migrations
+{
+    public partial class nullable_user : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "ViolationRegister",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "ServiceRegister",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "QueueRegister",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "AccountingRegister",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ViolationRegister_UserId",
+                table: "ViolationRegister",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceRegister_UserId",
+                table: "ServiceRegister",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QueueRegister_UserId",
+                table: "QueueRegister",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountingRegister_UserId",
+                table: "AccountingRegister",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccountingRegister_Users_UserId",
+                table: "AccountingRegister",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QueueRegister_Users_UserId",
+                table: "QueueRegister",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceRegister_Users_UserId",
+                table: "ServiceRegister",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ViolationRegister_Users_UserId",
+                table: "ViolationRegister",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccountingRegister_Users_UserId",
+                table: "AccountingRegister");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_QueueRegister_Users_UserId",
+                table: "QueueRegister");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceRegister_Users_UserId",
+                table: "ServiceRegister");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ViolationRegister_Users_UserId",
+                table: "ViolationRegister");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ViolationRegister_UserId",
+                table: "ViolationRegister");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServiceRegister_UserId",
+                table: "ServiceRegister");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QueueRegister_UserId",
+                table: "QueueRegister");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AccountingRegister_UserId",
+                table: "AccountingRegister");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "ViolationRegister",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "ServiceRegister",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "QueueRegister",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "AccountingRegister",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
