bc9183eb57815613b166a694596c7e3d765f10cb
George Svitenko <georg.svit@gmail.com>
2021-04-20 23:13:30 +0300

    Change tool domain class Add service status property, rename imgUri property

--- Domain/Tool.cs
+++ Domain/Tool.cs
@@ -10,13 +10,14 @@ namespace SmartLocker.WebAPI.Domain
         private Tool() { }
 
         [JsonConstructor]
-        public Tool(Guid id, string name, string description, string imgUri, AccessLevel accessLevel, Guid? userId, Guid? lockerId, Guid serviceBookId, Locker locker, User user, ServiceBook serviceBook)
+        public Tool(Guid id, string name, string description, string imgUri, AccessLevel accessLevel, string serviceState, Guid? userId, Guid? lockerId, Guid serviceBookId, Locker locker, User user, ServiceBook serviceBook)
         {
             Id = id;
             Name = name;
             Description = description;
-            ImgUri = imgUri;
+            ImgUrl = imgUri;
             AccessLevel = accessLevel;
+            ServiceState = serviceState;
             UserId = userId;
             LockerId = lockerId;
             ServiceBookId = serviceBookId;
@@ -30,33 +31,44 @@ namespace SmartLocker.WebAPI.Domain
             Id = Guid.NewGuid();
             Name = request.Name;
             Description = request.Description;
-            ImgUri = request.ImgUrl;
+            ImgUrl = request.ImgUrl;
             AccessLevel = (AccessLevel)request.AccessLevel;
             ServiceBook = new ServiceBook(Id, request.LastServiceDate, request.MsBetweenServices, request.MaxUsages, request.Usages);
             ServiceBookId = Id;
+            ServiceState = ServiceStates.SERVED;
         }
 
         public Tool(ToolEditRequest request)
         {
             Name = request.Name;
             Description = request.Description;
-            ImgUri = request.ImgUrl;
+            ImgUrl = request.ImgUrl;
             AccessLevel = (AccessLevel)request.AccessLevel;
         }
 
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImgUri { get; set; }
+        public string ImgUrl { get; set; }
         public AccessLevel AccessLevel { get; set; }
+        public string ServiceState { get; set; }
 
         public Guid? UserId { get; set; }
         public Guid? LockerId { get; set; }
         public Guid ServiceBookId { get; set; }
 
         //
+        [JsonIgnore]
         public Locker Locker { get; set; }
+        [JsonIgnore]
         public User User { get; set; }
         public ServiceBook ServiceBook { get; set; }
+
+
+        public void SetNull()
+        {
+            Locker = null;
+            User = null;
+        }
     }
 }
