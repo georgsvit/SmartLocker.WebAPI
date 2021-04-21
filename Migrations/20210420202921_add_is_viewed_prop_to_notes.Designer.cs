e0019208621ef5788bb1ee677f875820348c3c5f
George Svitenko <georg.svit@gmail.com>
2021-04-22 01:32:30 +0300

    Apply model change

--- Migrations/20210420202921_add_is_viewed_prop_to_notes.Designer.cs
+++ Migrations/20210420202921_add_is_viewed_prop_to_notes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartLocker.WebAPI.Data;
 
 namespace SmartLocker.WebAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210420202921_add_is_viewed_prop_to_notes")]
+    partial class add_is_viewed_prop_to_notes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210420202921_add_is_viewed_prop_to_notes.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartLocker.WebAPI.Migrations
+{
+    public partial class add_is_viewed_prop_to_notes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsViewed",
+                table: "ViolationRegister",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsViewed",
+                table: "ServiceRegister",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsViewed",
+                table: "QueueRegister",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsViewed",
+                table: "AccountingRegister",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsViewed",
+                table: "ViolationRegister");
+
+            migrationBuilder.DropColumn(
+                name: "IsViewed",
+                table: "ServiceRegister");
+
+            migrationBuilder.DropColumn(
+                name: "IsViewed",
+                table: "QueueRegister");
+
+            migrationBuilder.DropColumn(
+                name: "IsViewed",
+                table: "AccountingRegister");
+        }
+    }
+}
