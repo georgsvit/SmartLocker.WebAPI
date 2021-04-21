27bbc43fc8b5d9e8786101d13f0b570eeda23bc3
George Svitenko <georg.svit@gmail.com>
2021-04-22 01:33:43 +0300

    Add notification response dto

---
+++ Contracts/DTOs/External/Responses/NotificationsResponse.cs
@@ -0,0 +1,13 @@
+﻿using SmartLocker.WebAPI.Domain.RegisterNotes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Responses
+{
+    public record NotificationsResponse(
+        List<ServiceRegisterNote> ServiceNotes,
+        List<ViolationRegisterNote> ViolationNotes
+        );
+}
