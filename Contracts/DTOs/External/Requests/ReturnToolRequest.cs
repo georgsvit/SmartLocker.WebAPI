976bee62dca40d139d40ad4ccb7ea8c1e626b0c6
George Svitenko <georg.svit@gmail.com>
2021-06-23 13:40:37 +0300

    Add dto for tool return request

---
+++ Contracts/DTOs/External/Requests/ReturnToolRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLocker.WebAPI.Contracts.DTOs.External.Requests
+{
+    public record ReturnToolRequest(
+        [Required] Guid userId,
+        [Required] Guid toolId,
+        [Required] Guid lockerId,
+        [Required] DateTime date
+    );
+}
